2031f2811733c072b229e3c7edbe39f28f334ccd
vin14 <pasallimelvin@student.telkomuniversity.ac.id>
2022-03-11 17:49:14 +0700

    inisialisasi projek

---
+++ tpmodul3_1302204068/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos table_Kodepos = new KodePos();
+        Console.WriteLine("========= GET KODE POS KEBONWARU =========");
+        table_Kodepos.getKodepos("Kebonwaru");
+        Console.WriteLine("==== GET ALL KODE POS ====");
+        table_Kodepos.getAllkodepos();
+        /////////////////////////////////////
+
+        Console.WriteLine("========= KUNCI PINTU =========");
+        DoorMachine pintu = new DoorMachine();
+        pintu.kunci();
+    }
+
+}
+
+class KodePos
+{
+    Dictionary<string, string> dic = new Dictionary<string, string>()
+    {
+            {"Batununggal"  ,"40266"},
+            {"Kujangsari"   ,"40287"},
+            {"Mengger"      ,"40267"},
+            {"Wates"        ,"40256"},
+            {"Cijaura"      ,"40287"},
+            {"Jatisari"     ,"40286"},
+            {"Margasari"    ,"40286"},
+            {"Sekejati"     ,"40286"},
+            {"Kebonwaru"    ,"40272"},
+            {"Maleer"       ,"40274"},
+            {"Samoja"       ,"40273"}
+    };
+
+    public void getKodepos(string kel)
+    {
+        if (dic.ContainsKey(kel))
+        {
+            Console.WriteLine(kel + " : " + dic[kel]);
+        }
+        else
+        {
+            Console.WriteLine(kel + " tidak ditemukan");
+        }
+    }
+
+    public void getAllkodepos()
+    {
+        foreach (KeyValuePair<string, string> ele1 in dic)
+        {
+            Console.WriteLine("{0} \t\t {1}", ele1.Key, ele1.Value);
+        }
+    }
+
+
+}
+
+class DoorMachine
+{
+    enum State { TERKUNCI, TERBUKA };
+    public void kunci()
+    {
+        State state = State.TERKUNCI;
+
+        String[] screenName = { "TERKUNCI", "TERBUKA" };
+        do
+        {
+            Console.WriteLine("PINTU " + screenName[(int)state]);
+            Console.Write("Enter Command : ");
+            String command = Console.ReadLine();
+            switch (state)
+            {
+                case State.TERKUNCI:
+                    if (command == "Buka Pintu")
+                    {
+                        state = State.TERBUKA;
+                    }
+                    break;
+                case State.TERBUKA:
+                    if (command == "Kunci Pintu")
+                    {
+                        state = State.TERKUNCI;
+                    }
+                    break;
+
+
+            }
+        } while (state != State.TERKUNCI);
+    }
+}
